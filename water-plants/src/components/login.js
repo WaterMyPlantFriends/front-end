bebcc4fc6613a871b93a1f666bfbd373de21443e
Winifred Lu <winnie.lu@comcast.net>
2021-12-20 13:43:30 -0500

    login basics in place, still need styling and authentication

--- water-plants/src/components/login.js
+++ water-plants/src/components/login.js
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import * as yup from 'yup';
+import loginSchema from './validation/loginSchema';
+
+const initialFormValues = {
+    username: '',
+    password: '',
+}
+const initialFormErrors = {
+    username: '',
+    password: '',
+}
+const initialDisabled=true;
 
 export default function login(props) {
+    // state slices
+    const [formValues, setFormValues] = useState(initialFormValues);
+    const [formErrors, setFormErrors] = useState(initialFormErrors);
+    const [disabled, setDisabled] = useState(initialDisabled);
+    // functions
+    const validate = (name, value) => {
+        yup.reach(loginSchema, name)
+            .validate(value)
+            .then(() => setFormErrors({ ...formErrors, [name]: '' }))
+            .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0] }))
+    }
+    const inputChange = (name, value) => {
+        // validate
+        validate(name, value);
+        setFormValues({
+            ...formValues,
+            [name]: value,
+        })
+    }
+    const onChange = evt => {
+        const { name, value } = evt.target;
+        inputChange(name, value);
+    }
+    // update login button when formValues change
+    useEffect(() => {
+        loginSchema.isValid(formValues)
+            .then(valid => setDisabled(!valid));
+    }, [formValues]);
+    // return Login element
     return (
         <>
             <h2>Login</h2> 
             <div className='errors'>
                 {/* errors here */}
+                <div>{errors.username}</div>
+                <div>{errors.password}</div>
             </div>
             {/* Form */}
             <div className='form-container'>
@@ -31,6 +74,7 @@ export default function login(props) {
                         />
                     </label>
                 </div>
+                <button type='submit' id='loginBtn' disabled={disabled}>Login</button>
             </div>
         </>
     )
