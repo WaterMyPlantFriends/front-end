421840f68194f068c6eb4751d8158aaf588b6a6c
Krystle Mitchell <mitchell.krystle2@gmail.com>
2021-12-20 13:20:29 -0800

    All components are rendering

--- watermyplants/src/App.js
+++ watermyplants/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Signup from './components/Signup'
 import Login from './components/login'
+import Header from './components/Header'
 import {BroswerRouter as Router, Switch, Redirect} from 'react-router-dom'
 
 
@@ -13,8 +14,11 @@ function App() {
       <RouteContainer>
         <Switch>
           <Route exact path="/" component={Login} />
+          <Route exact path="/login" component={Login} />
           <Route exact path from="/signup" component={Signup} />
-          <PrivateRoute path="/" />
+          <PrivateRoute path="/header" component={Header} />
+          <PrivateRoute path="/profile" component={Profile} />
+
         </Switch>
       </RouteContainer>
 
