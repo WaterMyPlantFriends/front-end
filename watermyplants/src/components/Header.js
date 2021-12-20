bea8d1cb8202d453b147998cd8592eb991289657
lomelo_x <aalomelo@gmail.com>
2021-12-20 14:22:30 -0600

    WIP

--- watermyplants/src/components/Header.js
+++ watermyplants/src/components/Header.js
@@ -2,25 +2,41 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Link, } from "react-router-dom";
 
 export default function Header() {
+
+    const header = document.querySelector('div');
+    const headerContainer = document.createElement('div');
+    const titleContainer = document.createElement('div');
+    const headerTitle = document.createElement('h1')
+    const headerNav = document.createElement('nav');
+    const homeLink = document.createElement('a');
+    const loginLink = document.createElement('a');
+    const signupLink = document.createElement('a');
+    const profileLink = document.createElement('a')
     
+    header.classList.add('header')
+    headerContainer.classList.add('header-container')
+    titleContainer.classList.add('title-container')
+
+
+
+
+
+
+
+
+
     return (
         <div className='header'>
             <div className='header-container'>
                 <div className='title-container'>
-                    <h1 className='title-1'>WATER MY PLANTS</h1>
+                    <h1 className='site-title'>WATER MY PLANTS</h1>
                 </div>
-                <Router>
                 <nav>
-                    <Route exact path='/' />
-                    <Link to="/"><button id='home'>HOME</button></Link>
-                    <Route exact path='/' />
-                    <Link to="/"><button id='login'>LOGIN</button></Link>
-                    <Route exact path='/' />
-                    <Link to="/"><button id='signup'>SIGN UP</button></Link>
-                    <Route exact path='/' />
-                    <Link to="/"><button id='profile'>PROFILE</button></Link>
+                    <button id='home'>HOME</button>
+                    <button id='login'>LOGIN</button>
+                    <button id='signup'>SIGN UP</button>
+                    <button id='profile'>PROFILE</button>
                 </nav>
-                </Router>
             </div>
         </div>
     )
