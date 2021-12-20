428ce04fcfd0eaecf3500abf28b915fde6f7b557
Krystle Mitchell <mitchell.krystle2@gmail.com>
2021-12-20 12:28:27 -0800

    Rendering Signup Component to Appjs

--- watermyplants/src/App.js
+++ watermyplants/src/App.js
@@ -1,10 +1,22 @@
-
+import React from 'react';
 import './App.css';
+import Signup from './components/Signup'
+import {BroswerRouter as Router, Switch, Redirect} from 'react-router-dom'
+
 
 function App() {
   return (
+    
     <div className="App">
       <h1>Water My Plants</h1>
+      <RouteContainer>
+        <Switch>
+          <Route exact path="/" />
+          <Route exact path from="/signup" component={Signup} />
+          <PrivateRoute path="/" />
+        </Switch>
+      </RouteContainer>
+
    
     </div>
   );
