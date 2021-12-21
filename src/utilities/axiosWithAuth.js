f6eeca26bb7a12bd2b900c71231c93c22c7876ba
Brandyn <stateofw@gmail.com>
2021-12-21 15:18:55 -0500

    fixed comment

--- src/utilities/axiosWithAuth.js
+++ src/utilities/axiosWithAuth.js
@@ -4,7 +4,7 @@ const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
 
   return axios.create({
-    //this code makes it so all calls that use axiosWithAuth in the future don't need to include 'https://build-week-water-my-plants-1.herokuapp.com/api'
+    //this code makes it so all calls that use axiosWithAuth in the future don't need to include 'https://watermyplantz.herokuapp.com/api/'
     baseURL: "https://watermyplantz.herokuapp.com/api/",
     headers: {
       Authorization: token,
