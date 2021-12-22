fce82c66aefd2a362a103b3225db4ca9a7025823
James Calvo <15jcalvo@gmail.com>
2021-12-22 10:24:53 -0800

    store created

--- src/index.js
+++ src/index.js
@@ -5,11 +5,16 @@ import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import reducer from './reducers/index'
+import reducer from './reducers/index';
+
+const store = createStore(reducer);
+console.log(store.getState())
 
 ReactDOM.render(
+  <Provider store={store}>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
